8562cc0ec28a41b6641e7f2cd9db67acc2e827a6
ryan <ryan@ryan-All-Series>
2017-12-01 22:20:25 +0800

    Use properties with annotation @Value

--- src/main/java/com/luv2code/springdemo/SwimCoach.java
+++ src/main/java/com/luv2code/springdemo/SwimCoach.java
@@ -1,8 +1,24 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class SwimCoach implements Coach {
 
+	public String getEmail() {
+		return email;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
 	private FortuneService fortuneService;
+	
+	@Value("${foo.email}")
+	private String email;
+	
+	@Value("${foo.team}")
+	private String team;
 	
 	public SwimCoach(FortuneService theFortuneService) {
 		fortuneService = theFortuneService;
